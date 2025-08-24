49d231346eb45352a46a43302dbf37edf35e2dc9
dhViet2004 <22671321.viet@student.iuh.edu.vn>
2025-08-24 21:56:14 +0700

    Done bai 25

---
+++ Tuan01/Shape_describe/dist/Main.js
@@ -0,0 +1,8 @@
+"use strict";
+class Shape {
+    static describe() {
+        return "This is a shape.";
+    }
+}
+const description = Shape.describe();
+console.log(description);
---
+++ Tuan01/Shape_describe/src/Main.ts
@@ -0,0 +1,8 @@
+class Shape {
+    static describe(): string {
+        return "This is a shape.";
+    }
+}
+
+const description = Shape.describe();
+console.log(description);
