75f4ae3906d23ad35957995b69b1dc859da038df
dhViet2004 <22671321.viet@student.iuh.edu.vn>
2025-08-24 21:26:08 +0700

    Done bai 22

---
+++ Tuan01/Stack/dist/Main.js
@@ -0,0 +1,25 @@
+"use strict";
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(item) {
+        this.items.push(item);
+    }
+    pop() {
+        return this.items.pop();
+    }
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.isEmpty());
