2d01a6a6385037bf9b99442d0f14be6671323ca6
H82M19\Student <Student>
2025-08-30 14:22:55 +0700

    Done bai 18

---
+++ Tuan02/Bai18/src/index.ts
@@ -0,0 +1,11 @@
+async function fetchUser(id: number): Promise<{id: number; name: string}> {
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve({id, name: `User${id}`});
+        }, 1000);
+    })
+}
+
+fetchUser(1).then((user)=>{
+    console.log("User nhận được:", user);
+});
